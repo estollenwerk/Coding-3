4c4b69eceb0fc4f97cc716f9ff35e424c06240c3
estollenwerk <102555567+estollenwerk@users.noreply.github.com>
2025-09-03 22:37:43 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
-import java.util.Random;
 
 public class Main
 {
-	public static void main(String[] args) {
-		int[] tempArray = { 0, 0, 0, 0, 0,};
-		int [] salesRevenue = { 312, 589, 476, 328, 601, 445, 390, 523, 317, 578, 341, 499, 365, 420, 560, 385, 432, 511, 398, 600};
-		int maxRevenue = 0;
-		int nextRevenue = 602;
+    public static void TopFive(int[] tempArray,int[] salesRevenue, int maxRevenue, int nextRevenue) {
+        
 		for(int i = 0; i < tempArray.length; i++){
 		    for(int j = 0; j < salesRevenue.length; j++){
 		        if(salesRevenue[j] > maxRevenue && salesRevenue[j] < nextRevenue){
@@ -23,5 +19,14 @@ public class Main
 		for(int l = 0; l < tempArray.length; l++){
 		    System.out.println(tempArray[l]);
 		}
+    }
+    
+	public static void main(String[] args) {
+		int[] tempArray = { 0, 0, 0, 0, 0,};
+		int [] salesRevenue = { 312, 589, 476, 328, 601, 445, 390, 523, 317, 578, 341, 499, 365, 420, 560, 385, 432, 511, 398, 600};
+		int maxRevenue = 0;
+		int nextRevenue = 602;
+		TopFive(tempArray, salesRevenue, maxRevenue, nextRevenue);
+		}
 	}
-}
+
