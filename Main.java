f02e34093745b8e6f747d9562a043c479570b1d4
estollenwerk <102555567+estollenwerk@users.noreply.github.com>
2025-09-03 21:45:09 -0500

    uploaded second coding project

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class Main
+{
+	public static void main(String[] args) {
+		int[] tempArray = { 0, 0, 0, 0, 0,};
+		int [] salesRevenue = { 312, 589, 476, 328, 601, 445, 390, 523, 317, 578, 341, 499, 365, 420, 560, 385, 432, 511, 398, 600};
+		int maxRevenue = 0;
+		int nextRevenue = 602;
+		for(int i = 0; i < tempArray.length; i++){
+		    for(int j = 0; j < salesRevenue.length; j++){
+		        if(salesRevenue[j] > maxRevenue && salesRevenue[j] < nextRevenue){
+		            maxRevenue = salesRevenue[j];
+		            tempArray[i] = maxRevenue;
+		            
+		            
+		        }
+		    }
+		    nextRevenue = maxRevenue;
+		    maxRevenue = 0;
+		}
+		
+		for(int l = 0; l < tempArray.length; l++){
+		    System.out.println(tempArray[l]);
+		}
+	}
+}
